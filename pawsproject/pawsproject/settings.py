32ee875a090b7fe2ccf35592c60172845c872d4f
GAUSSES369 <mavergara2@eafit.edu.co>
2025-11-06 22:12:22 -0500

    Update settings.py

--- pawsproject/pawsproject/settings.py
+++ pawsproject/pawsproject/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-paws-and-walks')
 DEBUG = True
-ALLOWED_HOSTS = ['52.73.5.125']
+ALLOWED_HOSTS = ['54.91.180.208']
 
 INSTALLED_APPS = [
     'accounts',
